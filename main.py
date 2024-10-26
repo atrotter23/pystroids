4075fe7216d1aa206ea766e2cd03e09f3a92f0b1
Alex Trotter <astrotter23@gmail.com>
2024-10-26 09:11:39 -0700

    Now successfully draws character object to the screen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 def main():
     print("Starting asteroids!")
     
@@ -9,16 +9,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0     #delta time
     #loop for drawing to screen 
+    newPlayer = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black", rect=None, special_flags=0)
+        newPlayer.draw(screen)
         pygame.display.flip()
         # decouple game speed from computer speed
         dt = clock.tick(60) / 1000
-
+        
+        
 
 if __name__ == "__main__":
     main()
